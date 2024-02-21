c844ec2da53e39bb1388ce3dccc23938a2e72679
cansu <cansu.alaeddinoglu@slalom.com>
2024-02-21 10:56:45 -0800

    back to working

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifylearning": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
