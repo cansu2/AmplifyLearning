1be698148f717132dd7cdef5427f4dc60d3d48dd
cansu <cansu.alaeddinoglu@slalom.com>
2024-02-22 10:47:25 -0800

    working with api + auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,16 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
     }
+  },
+  "auth": {
+    "amplifylearning6afa5f4c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
   }
 }
--- index.js
+++ index.js
@@ -5,10 +5,10 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-// import { Amplify } from 'aws-amplify';
-// import amplifyconfig from './src/amplifyconfiguration.json';
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './src/amplifyconfiguration.json';
 
-// Amplify.configure(amplifyconfig);
+Amplify.configure(amplifyconfig);
 
 AppRegistry.registerComponent(appName, () => App);
 
